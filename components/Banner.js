5916c7e61762dda131fc9f8e8feb4839da0ae6a6
AneeshaRama <aneesharama@gmail.com>
2022-05-11 09:12:17 +0530

    Completed banner section

--- components/Banner.js
+++ components/Banner.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Image from "next/image";
 
 const Banner = () => {
   return (
     <>
-      <h1>Banner</h1>
+      <div className="relative h-[300px] sm:h-[400px] lg:h-[550px] xl:[600px] 2xl:h-[700px]">
+        <Image
+          src="https://links.papareact.com/0fm"
+          layout="fill"
+          objectFit="cover"
+        />
+        <div className="absolute top-[39%] w-full text-center text-gray-800 text-lg sm:text-xl font-semibold tracking-wide">
+          <p>Not sure where to go? Perfect</p>
+          <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-semibold sm:mt-4 mt-2 hover:shadow-xl active:scale-110 transition transform ease-in-out duration-200">
+            I'm flexible
+          </button>
+        </div>
+      </div>
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,12 @@ const Home = () => {
       <Header />
       {/* Banner */}
       <Banner />
+      {/* main */}
+      <main className="max-w-6xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+      </main>
     </>
   );
 };
