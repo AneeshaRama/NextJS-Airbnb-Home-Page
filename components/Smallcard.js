81c4efe8417e8d7dd53d645da3f258c39966917b
AneeshaRama <aneesharama@gmail.com>
2022-05-11 09:44:39 +0530

    Added small card component and API call

---
+++ components/Smallcard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "next/image";
+
+const Smallcard = ({ item }) => {
+  return (
+    <>
+      <div className="flex items-center m-2 mt-7 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-110 transition transform duration-200 ease-out">
+        <div className="relative h-16 w-16">
+          <Image className="rounded-lg" src={item.img} layout="fill" />
+        </div>
+        <div>
+          <h2>{item.location}</h2>
+          <h3 className="text-gray-500">{item.distance}</h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Smallcard;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Smallcard from "../components/Smallcard";
 
-const Home = () => {
+const Home = ({ exploreData, cardData }) => {
+  console.log(exploreData, cardData);
   return (
     <>
       {/* Header */}
@@ -12,11 +14,39 @@ const Home = () => {
       {/* main */}
       <main className="max-w-6xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
-          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <h2 className="text-4xl font-semibold pb-5 text-gray-800">
+            Explore Nearby
+          </h2>
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {exploreData?.map((item) => (
+              <Smallcard key={item.location} item={item} />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold text-gray-800 py-10">
+            Live Anywhere
+          </h2>
         </section>
       </main>
     </>
   );
 };
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+
+  const cardData = await fetch("https://links.papareact.com/zp1").then((res) =>
+    res.json()
+  );
+  return {
+    props: {
+      exploreData,
+      cardData,
+    },
+  };
+}
 
 export default Home;
